489798163e8c447207aae042b5e8ff79a87e6ca6
Saeed Abdulle <104086456+Saabdulle@users.noreply.github.com>
2024-01-04 15:52:15 +0000

    updated gitignore file

--- main.tf
+++ main.tf
@@ -1,28 +1,28 @@
-module "vpc" {
-  source       = "./modules/vpc"
-  cluster_name = var.cluster_name
-  vpc_name     = var.vpc_name
-  # public_subnet_cidr_blocks = var.public_subnet_cidr_blocks     # Uncomment to set specific cidr blocks
-  # private_subnet_cidr_blocks = var.private_subnet_cidr_blocks   # Uncomment to set specific cidr blocks
-}
+# module "vpc" {
+#   source       = "./modules/vpc"
+#   cluster_name = var.cluster_name
+#   vpc_name     = var.vpc_name
+#   # public_subnet_cidr_blocks = var.public_subnet_cidr_blocks     # Uncomment to set specific cidr blocks
+#   # private_subnet_cidr_blocks = var.private_subnet_cidr_blocks   # Uncomment to set specific cidr blocks
+# }
 
-module "security" {
-  source   = "./modules/security"
-  vpc_id   = module.vpc.vpc_id
-  vpc_name = var.vpc_name
-}
+# module "security" {
+#   source   = "./modules/security"
+#   vpc_id   = module.vpc.vpc_id
+#   vpc_name = var.vpc_name
+# }
 
-module "database" {
-  source   = "./modules/database"
-  vpc_security_group_ids = [module.security.web_sg_id]
-  public_subnets = module.vpc.public_subnets
-}
-module "eks_cluster" {
-  source          = "./modules/eks"
-  vpc_id          = module.vpc.vpc_id
-  private_subnets = module.vpc.private_subnets
-  cluster_name    = var.cluster_name
-}
+# module "database" {
+#   source   = "./modules/database"
+#   vpc_security_group_ids = [module.security.web_sg_id]
+#   public_subnets = module.vpc.public_subnets
+# }
+# module "eks_cluster" {
+#   source          = "./modules/eks"
+#   vpc_id          = module.vpc.vpc_id
+#   private_subnets = module.vpc.private_subnets
+#   cluster_name    = var.cluster_name
+# }
 module "ecr" {
   source = "./modules/ecr"
   docker_image_name = var.docker_image_name
--- modules/ecr/output.tf
+++
@@ -1,3 +0,0 @@
-output "ecr_repository_uri" {
-  value = aws_ecr_repository.backend_repo.repository_url
-}
