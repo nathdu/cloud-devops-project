7f06b38c4974c6acd9d62123f8460fbaea173378
Saeed Abdulle <104086456+Saabdulle@users.noreply.github.com>
2024-01-04 11:27:10 +0000

    updated the ecr resource

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -4,9 +4,11 @@ resource "aws_ecr_repository" "backend_repo" {
 
 # Push existing Docker image to ECR
 resource "null_resource" "push_to_ecr" {
+  # To authenticate AWS CLI, move the command in quotes into command block
+  # "aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin REPLACE-ME.dkr.ecr.eu-west-2.amazonaws.com"
+  # Repalce with your Account-id without hyphens
   provisioner "local-exec" {
     command = <<EOT
-    
       docker tag ${var.docker_image_name} ${aws_ecr_repository.backend_repo.repository_url}:1.0.0
       docker push ${aws_ecr_repository.backend_repo.repository_url}:1.0.0
     EOT
