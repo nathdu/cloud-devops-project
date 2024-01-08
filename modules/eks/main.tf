6eb63785c35260fa47fc0d294b5347c4392bc1d4
Saeed Abdulle <104086456+Saabdulle@users.noreply.github.com>
2024-01-08 14:29:13 +0000

    image tag updated to 31

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -5,6 +5,7 @@ module "eks" {
   cluster_version = "1.27"
   vpc_id                         = var.vpc_id
   subnet_ids                     = var.private_subnets
+  
   cluster_endpoint_public_access = true
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
