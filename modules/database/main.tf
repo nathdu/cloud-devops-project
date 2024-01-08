f0210697e1fb7c061ef62c554eaeb5817a1e149d
Saeed Abdulle <104086456+Saabdulle@users.noreply.github.com>
2024-01-08 12:29:57 +0000

    git ignore updated

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -1,25 +1,24 @@
-resource "aws_db_instance" "rds-db" {
+resource "aws_db_instance" "vapouropsdb" {
   allocated_storage = 20
-  identifier        = "testdb"
-  db_name           = "testdb"
+  identifier        = "vapouropsdb"
+  db_name           = "vapouropsdb"
   engine            = "postgres"
   engine_version    = "14.9"
-
   instance_class      = "db.t3.micro"
-  manage_master_user_password = true
-  master_user_secret_kms_key_id = aws_kms_key.example.key_id
+  # manage_master_user_password = true
+  # master_user_secret_kms_key_id = aws_kms_key.example.key_id
   username            = "vapourops"
-  # password            = data.aws_secretsmanager_secret_version.POSTGRES_PASSWORD.secret_string
+  password            = var.db_password
   skip_final_snapshot = true
   port                = 5432
-  # vpc_security_group_ids = var.vpc_security_group_ids
+  vpc_security_group_ids = var.vpc_security_group_ids
   db_subnet_group_name = aws_db_subnet_group.public_subnets.name
   publicly_accessible  = true
 }
 
-resource "aws_kms_key" "example" {
-  description = "Example KMS Key"
-}
+# resource "aws_kms_key" "example" {
+#   description = "Example KMS Key"
+# }
 
 # data "aws_secretsmanager_secret" "POSTGRES_USERNAME" {
 #   name = "POSTGRES_USERNAME"
