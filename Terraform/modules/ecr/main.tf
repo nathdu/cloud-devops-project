c35d8688e23d6adf5526fe831b7b03ec8928515d
Saeed Abdulle <104086456+Saabdulle@users.noreply.github.com>
2024-01-09 08:56:17 +0000

    reorganised files

---
+++ Terraform/modules/ecr/main.tf
@@ -0,0 +1,19 @@
+# provider "aws" {
+#     region = "us-east-1"
+# }
+# resource "aws_ecr_repository" "backend_repo" {
+#   name = "java-backend-server"
+# }
+
+# # Push existing Docker image to ECR
+# resource "null_resource" "push_to_ecr" {
+#   # To authenticate AWS CLI, move the command in quotes into command block
+#   # "aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin REPLACE-ME.dkr.ecr.eu-west-2.amazonaws.com"
+#   # Repalce with your Account-id without hyphens
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       docker tag ${var.docker_image_name} ${aws_ecr_repository.backend_repo.repository_url}:1.0.0
+#       docker push ${aws_ecr_repository.backend_repo.repository_url}:1.0.0
+#     EOT
+#   }
+# }
