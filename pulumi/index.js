04059f4ed68977e74ee58480676cfab158e2ea2a
YeshuaNun <145120313+YeshuaNun@users.noreply.github.com>
2024-01-10 13:37:42 +0000

    comment out route table module stuff

--- pulumi/index.js
+++ pulumi/index.js
@@ -8,12 +8,12 @@ const cluster = require("./modules/eks")
 const nodeGroup = require("./modules/nodeGroup")
 const db_instance = require("./modules/db_instance")
 const db_subnetGroup = require("./modules/db_subnetGroup")
-const {pubRouteTable,
-    pubRouteTableAssociation} = require("./modules/pubRouteTable")
-const {
-    pubSubnet1,
-    pubSubnet2,
-} = require("./modules/pubSubnets")
+// const {pubRouteTable,
+//     pubRouteTableAssociation} = require("./modules/pubRouteTable")
+// const {
+//     pubSubnet1,
+//     pubSubnet2,
+// } = require("./modules/pubSubnets")
 
 exports.vpcId=vpc.vpcId
 exports.vpcPrivSubnetIds = vpc.privateSubnetIds
